948fa13aee36d6142bed5cac8489e336d758ee81
uu4k <t.shimizu1115@gmail.com>
2019-05-03 09:53:39 +0900

    add auth middleware.

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -4,9 +4,51 @@ import Router from 'express-promise-router'
 import container from './config/ioc-config'
 import { ScouterController } from './controllers/scouter-controller'
 import * as bodyParser from 'body-parser'
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
 
 const app = express()
 const router = Router()
+
+interface AuthorizedRequest extends functions.https.Request {
+  user: admin.auth.DecodedIdToken
+}
+
+// https://github.com/firebase/functions-samples/blob/master/authenticated-json-api/functions/index.js
+//
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const authenticate = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  // GETのみ認証なしでもOK
+  if (req.method === 'GET') {
+    next()
+    return
+  }
+
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith('Bearer ')
+  ) {
+    res.status(403).send('Unauthorized')
+    return
+  }
+  const idToken = req.headers.authorization.split('Bearer ')[1]
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken)
+    ;(req as AuthorizedRequest).user = decodedIdToken
+    next()
+    return
+  } catch (e) {
+    res.status(403).send('Unauthorized')
+    return
+  }
+}
 
 app.use(
   bodyParser.urlencoded({
@@ -14,8 +56,11 @@ app.use(
   })
 )
 app.use(bodyParser.json())
-
+// TODO CORSちゃんと設定する
 app.use(cors({ origin: true }))
+
+app.use(authenticate)
+
 app.use('/', router)
 
 // TODO 認証確認
--- functions/tests/app.spec.ts
+++ functions/tests/app.spec.ts
@@ -5,6 +5,7 @@ import * as firebase from '@firebase/testing'
 import * as fs from 'fs'
 
 import * as admin from 'firebase-admin'
+import { ScouterController } from '../src/controllers/scouter-controller'
 
 const projectId = 'firestore-emulator-example'
 const rules = fs.readFileSync('../firestore.rules', 'utf8')
@@ -29,25 +30,35 @@ beforeEach(async () => {
 })
 
 afterAll(async () => {
-  await Promise.all(firebase.apps().map((app: any) => app.delete()))
+  await Promise.all(firebase.apps().map((fbapp: any) => fbapp.delete()))
 })
 
 describe('Test the root path', () => {
-  test('It should response the POST method', done => {
+  // TODO firebase authenticationの認証入れてテスト
+  test('It should response Non Authorized Error', done => {
     request(app)
       .post('/')
       .send({ title: 'aaa', description: 'bbb' })
       .then(async (response: any) => {
-        expect(response.statusCode).toBe(200)
-
-        const db = adminApp()
-        await db
-          .collection('scouters')
-          .get()
-          .then(querySnapshot => {
-            expect(querySnapshot.docs[0].data().title).toBe('aaa')
-          })
+        expect(response.statusCode).toBe(403)
         done()
       })
+  })
+})
+
+describe('Test Create Scouter', () => {
+  test('It shoud return success', async done => {
+    const controller = container.get(ScouterController)
+    await controller.createScouter('aaa', 'bbb').then(vm => {
+      expect(vm.toJson()).toEqual({})
+    })
+    const db = adminApp()
+    await db
+      .collection('scouters')
+      .get()
+      .then(querySnapshot => {
+        expect(querySnapshot.docs[0].data().title).toBe('aaa')
+      })
+    done()
   })
 })
